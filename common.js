5732aa90c15f8950841e92e4c926536cd001c8ed
Aliasvin <vincentnacario@gmail.com>
2025-11-05 08:36:23 +0100

    Add files via upload

--- common.js
+++ common.js
@@ -75,3 +75,20 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+const darkToggle = document.getElementById("darkModeToggle");
+
+// Herstel voorkeur
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  darkToggle.checked = true;
+}
+
+darkToggle.addEventListener("change", () => {
+  if (darkToggle.checked) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+});
